046fe59125c80ce606e937c83d62f604fbe5e5f6
cganelli <cganellishop@gmail.com>
2025-09-04 22:45:39 -0400

    Fix favicon configuration to use favicon.png

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,9 +40,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="icon" href="/favicon.png" type="image/png" />
+        <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+        <link rel="apple-touch-icon" href="/favicon.png" />
       </head>
       <body className={`${inter.variable} antialiased`}>
         {children}
